45b2c31fd6175b894e4543e8067426299c66e725
hrsuda1227 <hiroyuki.suda54@gmail.com>
2024-06-19 22:59:38 +0900

    neo-tree hidden file viewing

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
   },
   { import = "community" },
   { import = "plugins" },
+  { import = "user.plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "tokyonignt", "habamax" } },
