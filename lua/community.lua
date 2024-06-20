93eab8d04e36dc7b747008ae20db653ba95235cd
hrsuda1227 <hiroyuki.suda54@gmail.com>
2024-06-20 14:22:21 +0900

    added tmux wilder

--- lua/community.lua
+++ lua/community.lua
@@ -6,7 +6,10 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript-deno" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   -- import/override with your plugins folder
